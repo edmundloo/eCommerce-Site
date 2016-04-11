922263bb169001573e1bff88eecff35b7e168aef
Seth Kinchen <skinchen@uci.edu>
2016-04-11 14:08:58 -0700

    Almost complete homepage, added styles to style sheet, added js, and media for home page

--- js/global.js
+++ js/global.js
@@ -1,13 +1,15 @@
 var image1 = new Image()
-image1.src = "temp-1.jpg"
+image1.src = "media/slide-one.jpg"
 var image2 = new Image()
-image2.src = "temp-2.jpg"
+image2.src = "media/slide-two.jpg"
+var image3 = new Image()
+image3.src = "media/slide-three.jpg"
 
 var step=1;
 function slideit()
 {
 	document.images.slide.src = eval("image"+step+".src");
-	if(step<2)
+	if(step<3)
 		step++;
 	else
 		step=1;
